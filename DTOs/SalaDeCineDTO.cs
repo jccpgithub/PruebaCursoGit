315354f9af8b41a3b96d1a371cb3595174c34790
jccobos <spidersuperflash@gmail.com>
2022-10-17 03:06:27 +0200

    commit saladecineDTO

--- DTOs/SalaDeCineDTO.cs
+++ DTOs/SalaDeCineDTO.cs
@@ -7,7 +7,7 @@ namespace PeliculasAPI.DTOs
 {
 	public class SalaDeCineDTO
 	{
-		public int Id2 { get; set; }
+		public int Id3 { get; set; }
 		public string Nombre { get; set; }
 		public double Latitud { get; set; }
 		public double Longitud { get; set; }
