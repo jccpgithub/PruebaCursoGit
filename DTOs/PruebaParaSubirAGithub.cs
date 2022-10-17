294f83a9ac0e4d9f9e3aaa4148520e69210e9a03
jccpgithub <60964796+jccpgithub@users.noreply.github.com>
2022-10-17 12:54:57 +0200

    Update PruebaParaSubirAGithub.cs

--- DTOs/PruebaParaSubirAGithub.cs
+++ DTOs/PruebaParaSubirAGithub.cs
@@ -8,7 +8,7 @@ namespace PeliculasAPI.DTOs
 	public class PruebaParaSubirAGithub
 	{
 		public int Id { get; set; }
-		public string funcion { get; set; }
+		public string funcionpabajo { get; set; }
 		public double Latitud { get; set; }
 		public double Longitud { get; set; }
 	}
