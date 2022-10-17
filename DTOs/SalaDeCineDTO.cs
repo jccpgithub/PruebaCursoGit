9bb417516adc1a52c6b00af01e40c9c26a1045bf
jccobos <spidersuperflash@gmail.com>
2022-10-17 03:27:38 +0200

    bfgdgdfgdfdsda

--- DTOs/SalaDeCineDTO.cs
+++ DTOs/SalaDeCineDTO.cs
@@ -8,7 +8,7 @@ namespace PeliculasAPI.DTOs
 	public class SalaDeCineDTO
 	{
 		public int Id { get; set; }
-		public string Nombres { get; set; }
+		public string Nombresss { get; set; }
 		public double Latitud { get; set; }
 		public double Longitud { get; set; }
 	}
